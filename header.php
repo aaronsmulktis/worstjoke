edaab9ce56bc9f55ec984c4c8b97a0aac072578f
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-02-06 15:33:03 -0500

    .

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 		          <script type='text/javascript' src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
 		<![endif]-->
 
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+		<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script> -->
 		<script src="<?php echo get_template_directory_uri(); ?>/library/js/jquery.isotope.min.js"></script>
 		<script src="<?php echo get_template_directory_uri(); ?>/library/js/jquery.socialist.js"></script>
 
